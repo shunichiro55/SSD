b60b8296941fe14d630491f6cc0a1ef8ebbc4053
shunichiro <shunichiro.takemura@force-corp.co.jp>
2020-04-23 15:26:32 +0900

    js

---
+++ js/if-for.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let testScore = 60;
+
+if(testScore >= 80){
+    document.write("合格です！おめでとう！");
+}else if(testScore>=70){
+    document.write("惜しい！あと少し頑張って！");
+}else{
+    document.write("不合格です！勉強して再挑戦しましょう！")
+}
+
+// let i = 2;
+// if(i===1){
+//     console.log("真です");
+// }
---
+++ js/jspractice.js
@@ -0,0 +1,48 @@
+// document.write("Hello World!");
+// alert("Hello World!");
+
+// console.log("Hello Wodld!"+"田中さん");
+// console.log(10+ 10);
+// console.log("10"+"10");
+
+// console.log(1+1);
+// console.log(1-1);
+// console.log(2*2);
+// console.log(10/3);
+// console.log(10%3);
+
+// let x =1;
+// let y =1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// let x =7;
+// let y =x++;
+// let x2 =7;
+// let y2 =++x2;
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+'use strict'
+
+// let age = 30;
+// const name ="田中太郎";
+
+// age = age + 2;
+
+// console.log(`${name}さんの年齢は${age}です`);
+
+let testScore = 60;
+
+if(testScore >= 80){
+    document.write("合格です！おめでとう！");
+}else if(testScore>=70){
+    document.write("惜しい！あと少し頑張って！");
+}else{
+    document.write("不合格です！勉強して再挑戦しましょう！")
+}
